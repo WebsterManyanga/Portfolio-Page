6433028938ad3c1ba78dcca4dd126b601a24ebce
Webster <strategywebstermanyanga@gmail.com>
2023-04-26 13:47:49 +0200

    added functional menu bar for mobile

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuBar = document.querySelector('.top-nav');
+const menuLinks = menuBar.children;
+
+for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener('click', menuSlide);
+}
+
 menuBtn.addEventListener('click', menuSlide);
 
 function menuSlide() {
   if (menuBtn.firstElementChild.classList.contains('fa-bars')) {
       menuBtn.firstElementChild.classList.remove('fa-bars');
       menuBtn.firstElementChild.classList.add('fa-house');
+      menuBar.style.transform = 'translateX(0)';
+      
   } else {
       menuBtn.firstElementChild.classList.remove('fa-house');
       menuBtn.firstElementChild.classList.add('fa-bars');
-
+      menuBar.style.transform = 'translateX(-100vw)';
   }
 
 }
