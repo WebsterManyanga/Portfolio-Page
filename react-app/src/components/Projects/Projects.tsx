8aed6994572c67d4eda837386b14279f598b2629
Webster <strategywebstermanyanga@gmail.com>
2023-05-31 21:53:11 +0200

    restructured Projects into own component

--- react-app/src/components/Projects/Projects.tsx
+++ react-app/src/components/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import EminemTributePreview from '../../images/eminem-tribute-preview.jpg';
 import PortfolioPreview from '../../images/portfolio-page-preview.jpg';
 import LittleLemonPreview from '../../images/little-lemon.jpg';
 import ProjectData from '../../Server/Projects';
+import TechStack from '../TechStack';
 
 
 export default function Projects() {
@@ -12,19 +13,15 @@ export default function Projects() {
     <section className="main__section projects" id="projects">
     <h1>Projects</h1>
     <ul className="projects__list">
-      {ProjectData.map(project => (
-              <li className="projects__list-item">
+      {ProjectData.map((project, index) => (
+              <li className="projects__list-item" key={index}>
               <img 
                 src={project.image} 
                 alt="website preview image"
               />
               <h2>{project.name}</h2>
               <div className="projects__line"></div>
-              <ul className="projects__technologies">
-                <li><i className="fa-brands fa-html5"></i></li>
-                <li><i className="fa-brands fa-css3-alt"></i></li>
-                <li><i className="fa-brands fa-js"></i></li>
-              </ul>
+              <TechStack project={project} />
               <div className="projects__links">
                 <a href={project.site} className="button-link">
                   Visit Site
