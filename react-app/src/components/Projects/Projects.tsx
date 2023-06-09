9350ea68bba1618b245a8f936795f32bb2c93744
Webster <strategywebstermanyanga@gmail.com>
2023-06-09 08:44:33 +0200

    checkout

--- react-app/src/components/Projects/Projects.tsx
+++ react-app/src/components/Projects/Projects.tsx
@@ -1,11 +1,7 @@
 import './Projects.css';
-import UniversityPreview from '../../images/university-page-preview.jpg';
-import LibraryFormPreview from '../../images/library-form-preview.jpg';
-import EminemTributePreview from '../../images/eminem-tribute-preview.jpg';
-import PortfolioPreview from '../../images/portfolio-page-preview.jpg';
-import LittleLemonPreview from '../../images/little-lemon.jpg';
 import ProjectData from '../../Server/Projects';
 import TechStack from '../TechStack';
+import Card from 'react-bootstrap/Card';
 
 
 export default function Projects() {
@@ -14,27 +10,22 @@ export default function Projects() {
     <h1>Projects</h1>
     <ul className="projects__list">
       {ProjectData.map((project, index) => (
-              <li className="projects__list-item" key={index}>
-              <img 
-                src={project.image} 
-                alt="website preview image"
-              />
-              <h2>{project.name}</h2>
-              <div className="projects__line"></div>
-              <TechStack project={project} />
-              <div className="projects__links">
-                <a href={project.site} className="button-link">
-                  Visit Site
-                </a>
-                <a
-                  href={project.github} 
-                  className="button-link"
-                  target="_blank"
-                >
-                  Github Repo
-                </a>
-              </div>
-            </li>
+          <Card>
+            <Card.Img 
+              variant='top' 
+              src={project.image} 
+              alt='website preview image'
+            />     
+            <Card.Body>
+              <Card.Title>{project.name}</Card.Title>
+              <Card.Body>
+                <TechStack project={project}/>
+              </Card.Body>
+              <Card.Link href={project.site}>Visit Site</Card.Link>
+              <Card.Link href={project.github}>Github Repo</Card.Link>
+            </Card.Body>
+
+          </Card>
       ))}
     </ul>
   </section>
